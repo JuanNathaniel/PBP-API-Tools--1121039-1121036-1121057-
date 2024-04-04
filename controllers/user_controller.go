7aee661cf21101a9d70461156ce258e6768d59a3
Juan Nathaniel <joannanthaniel@gmail.com>
2024-04-05 00:06:56 +0700

    halo

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -1,1 +1,26 @@
-package controllers
+package main
+
+import (
+	"crypto/tls"
+
+	"gopkg.in/gomail.v2"
+)
+
+// send email
+func main() {
+	d := gomail.NewDialer("joannanthaneil@gmail.com", 587, "Juan Nathaniel", "123456")
+	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+
+	// Send emails using d.
+	m := gomail.NewMessage()
+	m.SetHeader("From", "joannanthaniel@gmail.com")
+	m.SetHeader("To", "if-21039@students.ithb.ac.id")
+	m.SetHeader("Subject", "Test email su")
+	m.SetBody("text/plain", "Hello, this is a test email!")
+
+	// Kirim email
+	if err := d.DialAndSend(m); err != nil {
+		panic(err)
+	}
+
+}
